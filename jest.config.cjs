e23dc3cd13c06da1f4c8b12b57ed36be6fa2b500
Gustavo <gustavomarquesm@gmail.com>
2025-07-17 17:54:33 -0300

    chore: adiciona configuração do Codecov com PR checks

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,10 +4,10 @@ module.exports = {
   coverageReporters: ['lcov', 'text'],
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
 };
